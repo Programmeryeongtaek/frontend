4c5b4bc005940ce80d87f1cd2ca5d2f4c10c99ef
오영택 <87858689+Programmeryeongtaek@users.noreply.github.com>
2022-06-25 16:56:55 +0900

    0625

--- 노마드 코더/JS로 크롬 앱 만들기/app3.js
+++ 노마드 코더/JS로 크롬 앱 만들기/app3.js
@@ -6,4 +6,15 @@ const h1 = document.querySelector(".hello h1");
 function handleTitleClick() {
     h1.className = "active";
 }
+
+function handleMouseEnter() {
+    h1.className = "active2";
+}
+
+function handleMouseLeave() {
+    h1.className = "active3";
+}
+
 h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseEnter", handleMouseEnter);
+h1.addEventListener("mouseLeave", handleMouseLeave);
