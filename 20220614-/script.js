e203d8200d5e5c6193ef7d1f60bc6868f20265ab
오영택 <87858689+Programmeryeongtaek@users.noreply.github.com>
2022-07-02 10:44:53 +0900

    0702

---
+++ 20220614-/script.js
@@ -0,0 +1,41 @@
+let spacePressed = false;
+let pressTime = null;
+
+const spacebar = document.querySelector('.space')
+const bar = document.querySelector('.bar')
+
+const update = () => {
+    if (spacePressed) {
+        spacebar.classList.add('active');
+    } else {
+        spacebar.classList.remove('active');
+        const diff = new Date().getTime() - pressTime.getTime();
+
+        bar.style.width = `${Math.max(0.001, Math.min(diff, 1200)) / 2}px`;        
+        bar.style.background = 
+            diff > 800
+                ? 'red'
+                : diff > 900
+                ? 'yellow'
+                : diff > 1100
+                ? 'green'
+                : diff > 1200
+                ? 'yellow'
+                : 'red';
+    }
+};
+
+addEventListener('keydown', (e) => {
+    if (e.key === ' ' && !spacePressed) {
+        spacePressed = true;
+        pressTime = new Date();
+        update();
+    }
+});
+
+addEventListener('keyup', (e) => {
+    if (e.key === ' ') {
+        spacePressed = false;
+        update();
+    }
+})
---
+++ 20220614-/script2.js
@@ -0,0 +1,41 @@
+let spacePressed = false;
+let pressTime = null;
+
+const spacebar = document.querySelector('.space')
+const bar = document.querySelector('.bar')
+
+const reflection = () => {
+    if (spacePressed) {
+        spacebar.classList.add('active');
+    } else {
+        spacebar.classList.remove('active');
+        const diff = new Date().getTime() - pressTime.getTime();
+
+        bar.style.width = `${Math.max(0.001, Math.min(diff, 1200)) / 2}px`
+        bar.style.background = 
+            diff > 800
+                ? 'red'
+                : diff > 900
+                ? 'yellow'
+                : diff > 1100
+                ? 'green'
+                : diff > 1200
+                ? 'yellow'
+                : 'red';
+    }
+};
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === ' ' && !spacePressed) {
+        spacePressed = true;
+        pressTime = new Date();
+        reflection();
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    if (e.key === ' ') {
+        spacePressed = false;
+        reflection();
+    }
+})
